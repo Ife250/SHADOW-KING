37202e715ff249b1d1b8b6aebe97aec6c29f5131
Kingsley <100943735+Ife250@users.noreply.github.com>
2022-04-29 21:01:56 +0200

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -10,7 +10,7 @@ Asena.addCommand({pattern: 'menu', fromMe: false, desc: Lang.WP}, (async (messag
     var r_text = new Array ();
     
     
-    r_text[0] = "https://ibb.co/qNxHGqL";
+    r_text[0] = "https://i.ibb.co/48Jbyvw/Kingsley.jpg";
     
     
     var i = Math.floor(1*Math.random())
